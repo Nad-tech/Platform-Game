033f17732aa310cdeced9f7eec076f47b16e2165
Nad-tech <68064836+Nad-tech@users.noreply.github.com>
2020-10-06 16:27:52 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,147 +1,250 @@
-#include<\Users\dexte\source\repos\new projects\Platform Game\Platform Game\GUI_Headers\ConsoleWindow.h>
-#include<\Users\dexte\source\repos\new projects\Platform Game\Platform Game\GUI_Headers\FrameBuffer.h>
-#include<string>
+#include<\Users\dexte\source\repos\new stuff\PlatformGameV2\PlatformGameV2\ConsoleWindow.h>
+#include<vector>
 #include<fstream>
-#include<iostream>
 #include<chrono>
-
-
+#include<string>
 
 ConsoleWindow cWindow;
-FrameBuffer camera;
-FrameBuffer world;
-ifstream inFile;
-float cameraX = 0;
-float cameraY = 0;
-
-FrameBuffer player;
-float playerX = 5;
-float playerY = (float)camera.height / 2;
-int height = 0;
-float speed = 30;
-
+bool quit = false;
 auto tp1 = chrono::system_clock::now();
 auto tp2 = chrono::system_clock::now();
 
+bool spaceBarPressed = false;
 
-void setPixel(int x, int y, short chr, short col, FrameBuffer &f) {
-	f.setCellChrCol(y*f.width + x, chr, col);
-	if (f.colours[y*f.width + x] == 0x0) {
-		f.cellType[y*f.width + x] = 'g';
-	}
-}
+struct Frame {
+	int width;
+	int height;
+	int size;
+	vector<short> chrs;
+	vector<short> cols;
+};
 
-void updateCamera() {
-	int worldIdx = (int)cameraX;
-	for (int i = 0; i < camera.size; i++) {
-		camera.setCellChrCol(i, world.characters[worldIdx], world.colours[worldIdx]);
-		if (i % camera.width == camera.width - 1) {
-			worldIdx += world.width - camera.width;
-		}
-		worldIdx++;
-	}
-}
-	
-void writeCamera() {
-	int x = 0;
-	int y = 0;
-	for (int i = 0; i < camera.size; i++) {
-		x++;
-		if (i % camera.width == 0) {
-			x = 0;
-			y++;
-		}
-	
-		cWindow.setPixel(
-			(y + camera.y)*SCREEN_WIDTH + (x + camera.x),
-			camera.characters[i], camera.colours[i]
-		);
-	}
-}
+Frame world;
 
-void writePlayer() {
-	player.x = (int)playerX;
-	player.y = (int)playerY;
+int sceneWidth;
+int sceneHeight;
+int sceneSize;
+
+int cameraWidth;
+int cameraHeight;
+float cameraX = 0;
+int cameraXvel = 0;
+int cameraScrPosX = 1;
+int cameraScrPosY = 1;
+
+Frame player;
+int playerWidth;
+int playerHeight;
+float playerX;
+float playerY;
+int playerXvel = 0;
+int playerYvel = 0;
+float velocityIncrement = 0.3;
+
+Frame enemy;
+float enemyX;
+float enemyY;
+int enemyHeight;
+
+void displayPlayer() {
+	int si = (int)playerY * SCREEN_WIDTH + (int)playerX;
 	
-	int x = 0;
-	int y = 0;
 	for (int i = 0; i < player.size; i++) {
-		x++;
-		if (i % player.width == 0) {
-			x = 0;
-			y++;
+		if(player.chrs[i] != 'A') cWindow.setPixel(si, player.chrs[i], player.cols[i]);
+		if (i % player.width == player.width - 1) {
+			si += SCREEN_WIDTH - player.width;
 		}
-
-		cWindow.setPixel(
-			(y + player.y)*SCREEN_WIDTH + (x + player.x),
-			player.characters[i], player.colours[i]
-		);
+		si++;
 	}
 }
 
-FrameBuffer loadFrameBuffer(string fileName) {
-	FrameBuffer frameBuffer;
+void displayCameraView() {
+	int wi = (int)cameraX;
+	int sx = 0;
+	int sy = 0;
 	
-	string directory = "data/";
-	string fileExtension = ".dan";
+	for (int i = 0; i < sceneSize; i++) {
+		cWindow.setPixel((sy + cameraScrPosY)*SCREEN_WIDTH + (sx + cameraScrPosX), world.chrs[wi], world.cols[wi]);
+		sx++;
+		wi++;
+		if (i % cameraWidth == cameraWidth - 1) {
+			wi += world.width - cameraWidth;
+			sx = 0;
+			sy++;
+		}
+	}
+}
 
-	inFile.open(directory + fileName + fileExtension);
+Frame loadPlayerFile(string fileName) {
+	Frame player;
+	ifstream inFile;
+	inFile.open("data/" + fileName + ".dan");
 
-	vector<short> characters;
-	vector<short> colours;
+	if (!inFile) {
+		string s = "Cant open " + fileName;
+		for (int i = 0; i < s.length(); i++) {
+			cWindow.setPixel(i, s[i], BG_RED | FG_WHITE);
+		}
+		cWindow.upDateWindow();
+		return player;
+	}
+
+	short width;
+	short height;
+
+	inFile >> width >> height;
+	player.width = width;
+	player.height = height;
+	player.size = width * height;
+
 	short chr;
 	short col;
-	int width, height;
+	while (inFile >> chr >> col) {
+		player.chrs.push_back(chr);
+		col == (BG_BLACK | FG_BLACK) ? col = (BG_BLUE | FG_GREEN) : col;
+		player.cols.push_back(col);
+	}
+
+	return player;
+}
+
+Frame loadSceneFile(string fileName) {
+	Frame scene;
+	ifstream inFile;
+	inFile.open("data/" + fileName + ".dan");
+
+	if (!inFile) {
+		string s = "Cant open " + fileName;
+		for (int i = 0; i < s.length(); i++) {
+			cWindow.setPixel(i, s[i], BG_RED | FG_WHITE);
+		}
+		cWindow.upDateWindow();
+		return scene;
+	}
+
+	short width;
+	short height;
 
 	inFile >> width >> height;
 
+	sceneWidth = width;
+	sceneHeight = height;
+	sceneSize = width * height;
+
+	scene.width = width;
+	scene.height = height;
+	scene.size = width * height;
+
+	cameraWidth = scene.width;
+	cameraHeight = scene.height;
+	
+	short chr;
+	short col;
 	while (inFile >> chr >> col) {
-		characters.push_back(chr);
-		colours.push_back(col);
+		scene.chrs.push_back(chr);
+		scene.cols.push_back(col);
 	}
 
 	inFile.close();
 
-	frameBuffer = FrameBuffer(width, height, 0, 0);
+	return scene;
+}
 
-	for (int i = 0; i < int(characters.size()); i++) {
-		frameBuffer.characters[i] = characters[i];
-		frameBuffer.colours[i] = colours[i];
+void buildWorldFromScenes() {
+	vector<Frame> scenes;
+	scenes.push_back(loadSceneFile("BG1"));
+	scenes.push_back(loadSceneFile("BG2"));
+	scenes.push_back(loadSceneFile("BG3"));
+	scenes.push_back(loadSceneFile("BG4"));
+
+	world.height = sceneHeight;
+	world.width = sceneWidth * scenes.size();
+	world.size = world.width * world.height;
+	world.chrs = vector<short>(world.size);
+	world.cols = vector<short>(world.size);
+	
+	int ww = world.width;
+	int sw = scenes[0].width;
+
+	for (int j = 0; j < 4; j++) {
+		for (int x = 0; x < scenes[j].width; x++) {
+			for (int y = 0; y < scenes[j].height; y++) {
+				world.chrs[y * ww + j * sw + x] = scenes[j].chrs[y*sw + x];
+				world.cols[y * ww + j * sw + x] = scenes[j].cols[y*sw + x];
+			}
+		}
+	}
+}
+
+struct Bullet {
+	float x = 0;
+	float y = 0;
+	int velIncrement = 200;
+	int col = BG_YELLOW | FG_RED;
+	int chr = QUARTER;
+	bool active = false;
+	int travelDistance = 0;
+};
+Bullet bullet;
+
+void shoot() {
+	if (!bullet.active) {
+		bullet.active = true;
+		bullet.x = playerX + 10;
+		bullet.y = playerY + 3;
+	}
+}
+
+void moveAndDisplayBullet(float elapsedTime) {
+	if (bullet.active) {
+		cWindow.setPixel(int(bullet.y) * SCREEN_WIDTH + int(bullet.x), bullet.chr, bullet.col);
+		bullet.x += bullet.velIncrement * elapsedTime;
+		bullet.travelDistance++;
+		if (bullet.x > cameraWidth) {
+			bullet.active = false;
+			bullet.travelDistance = 0;
+		}
+	}
+}
+
+void moveAndDisplayEnemy(float elapsedTime) {
+	enemyX -= (rand() % 50 + 10) * elapsedTime;
+	if (enemyX < 1) {
+		enemyX = cameraWidth - enemy.width;
+		enemyY = rand() % (cameraHeight - enemyHeight) + 1;
 	}
 
-	return frameBuffer;
+	int si = (int)enemyY * SCREEN_WIDTH + (int)enemyX;
+
+	for (int i = 0; i < enemy.size; i++) {
+		if (enemy.cols[i] != BG_WHITE && enemyX < cameraWidth) cWindow.setPixel(si, enemy.chrs[i], enemy.cols[i]);
+		if (i % enemy.width == enemy.width - 1) {
+			si += SCREEN_WIDTH - enemy.width;
+		}
+		si++;
+	}
 }
 
 int main() {
 	cWindow = ConsoleWindow(
 		"Dans Platform Game", SCREEN_WIDTH, SCREEN_HEIGHT, FONT_WIDTH, FONT_HEIGHT
 	);
-
-	FrameBuffer scr_1 = loadFrameBuffer("BG1");
-	FrameBuffer scr_2 = loadFrameBuffer("BG2");
-	FrameBuffer scr_3 = loadFrameBuffer("BG3");
-	FrameBuffer scr_4 = loadFrameBuffer("BG4");
-
-	int worldWidth = 4 * scr_1.width;
-	int worldHeight = scr_1.height;
-
-	camera = FrameBuffer(scr_1.width, scr_1.height, 1, 0);
-
-	world = FrameBuffer(worldWidth, worldHeight, 0, 0);
-
-	player = loadFrameBuffer("ROBOT");
-
-	for (int x = 0; x < scr_1.width; x++) {
-		for (int y = 0; y < scr_1.height; y++) {
-			setPixel(x, y, scr_1.characters[y*scr_1.width + x], scr_1.colours[y*scr_1.width + x], world);
-			setPixel(x + scr_1.width, y, scr_2.characters[y*scr_2.width + x], scr_2.colours[y*scr_1.width + x], world);
-			setPixel(x + 2 * scr_1.width, y, scr_3.characters[y*scr_1.width + x], scr_3.colours[y*scr_1.width + x], world);
-			setPixel(x + 3 * scr_1.width, y, scr_4.characters[y*scr_1.width + x], scr_4.colours[y*scr_1.width + x], world);
-		}
-	}
 	
-	bool quit = false;
-		
+	buildWorldFromScenes();
+
+	player = loadPlayerFile("SHIPWITHALPHA");
+	playerX = 10;
+	playerY = 10;
+
+	enemy = loadPlayerFile("ROBOT");
+	enemyX = cameraWidth - enemy.width;
+	enemyY = 5;
+	enemyHeight = enemy.height;
+
+	std::thread cameraThread;
+	std::thread windowThread;
+	std::thread playerThread;
+
 	while (!quit) {
 		tp2 = chrono::system_clock::now();
 		chrono::duration<float> elapsedTime = tp2 - tp1;
@@ -150,47 +253,86 @@ int main() {
 
 		if (GetAsyncKeyState(VK_ESCAPE) & 0x8000) quit = true;
 
-
 		if (GetAsyncKeyState(VK_RIGHT) & 0x8000) {
-			if (player.x < camera.width/2) playerX += speed * fElapsedTime;
-			else {
-				if (cameraX < 4 * scr_1.width - camera.width) {
-					cameraX += speed * fElapsedTime;
-				}
-			}
+			playerXvel++;
 		}
-
+		else {
+			if(playerXvel > 0) playerXvel--;
+			if(cameraXvel > 0) cameraXvel--;
+		}
+			
 		if (GetAsyncKeyState(VK_LEFT) & 0x8000) {
-			if (player.x > camera.x + camera.width/3) playerX -= speed * fElapsedTime;
-			else {
-				if (cameraX > 0) {
-					cameraX -= speed * fElapsedTime;
-				}
-			}
+			playerXvel--;
+		}
+		else {
+			if (playerXvel < 0) playerXvel++;
+			if (cameraXvel < 0) cameraXvel++;
 		}
 		
-		//Jump
 		if (GetAsyncKeyState(VK_UP) & 0x8000) {
-			if (player.y > camera.y + 1) {
-				playerY -= speed * 2 * fElapsedTime;
-				height++;
-			}
+			playerYvel--;
+		}
+		else {
+			if (playerYvel < 0) playerYvel++;
 		}
 
-		//Gravity
-		if (
-			world.cellType[(player.y + player.height)*world.width + player.x] != 'g' && 
-			world.cellType[(player.y + player.height)*world.width + (player.x + player.width - 1)] != 'g') 
-		{
-			if (player.y + player.height < camera.y + camera.height - 1) {
-				playerY += speed * fElapsedTime;
-			}
+		if (GetAsyncKeyState(VK_DOWN) & 0x8000) {
+			playerYvel++;
+		}
+		else{
+			if (playerYvel > 0) playerYvel--;
 		}
 
-		updateCamera();
-		writeCamera();
-		writePlayer();
+		playerX += playerXvel * velocityIncrement * fElapsedTime;
+		playerY += playerYvel * velocityIncrement * fElapsedTime;
+		
+		cameraX += 150 * velocityIncrement * fElapsedTime;
+			
+
+		if (cameraX + cameraWidth > world.width) {
+			cameraX = 0;
+		}
+
+		int screenOffset = 10;
+		if (playerX > (cameraScrPosX + cameraWidth) - player.width - screenOffset) {
+			playerX = (cameraScrPosX + cameraWidth) - player.width - screenOffset;
+			playerXvel = 0;
+		}
+
+		if (playerX < cameraScrPosX + screenOffset) {
+			playerX = cameraScrPosX + screenOffset;
+			playerXvel = 0;
+		}
+		
+		if (playerY > (cameraScrPosY + cameraHeight) - player.height + 1) {
+			playerY = (cameraScrPosY + cameraHeight) - player.height;
+			playerYvel = 0;
+		}
+
+		if (playerY < cameraScrPosY) {
+			playerY = cameraScrPosY;
+			playerYvel = 0;
+		}
+		
+		displayCameraView();
+
+		if (GetAsyncKeyState(VK_SPACE) & 0x8000) {
+			shoot();
+		}
+		
+		moveAndDisplayEnemy(fElapsedTime);
+		moveAndDisplayBullet(fElapsedTime);
+		displayPlayer();
 		cWindow.upDateWindow();
+
+		if (
+			(int)bullet.x > (int)enemyX && (int)bullet.x < (int)enemyX + enemy.width &&
+			((int)bullet.y > (int)enemyY && (int)bullet.y < (int)enemyY + enemy.height)
+			) 
+		{
+			cameraXvel++;
+			enemyX = cameraWidth - enemy.width;
+			enemyY = rand() % (cameraHeight - enemyHeight) + 1;
+		}
 	}
-	return 0;
 }
